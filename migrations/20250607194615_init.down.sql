cf4fe20ac78b2659d14d11145929ca53b946b5bc
santhosh7403 <skumar7403@gmail.com>
2026-06-30 06:59:48 +0530

    refactor: update migration down script to correctly drop dependent and FTS tables in SQLite

--- migrations/20250607194615_init.down.sql
+++ migrations/20250607194615_init.down.sql
@@ -1,6 +1,19 @@
-DROP TABLE IF EXISTS Users CASCADE;
+-- Disable foreign keys temporarily to prevent drop conflicts
+PRAGMA foreign_keys = OFF;
+
+-- Drop Virtual Tables first (FTS5)
+DROP TABLE IF EXISTS articles_fts;
+DROP TABLE IF EXISTS articletags_fts;
+
+-- Drop Junction / Dependent Tables
 DROP TABLE IF EXISTS Follows;
-DROP TABLE IF EXISTS Articles CASCADE;
 DROP TABLE IF EXISTS ArticleTags;
 DROP TABLE IF EXISTS FavArticles;
 DROP TABLE IF EXISTS Comments;
+
+-- Drop Base Tables
+DROP TABLE IF EXISTS Articles;
+DROP TABLE IF EXISTS Users;
+
+-- Re-enable foreign key checks
+PRAGMA foreign_keys = ON;
